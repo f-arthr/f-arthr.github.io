909aece3887664b3840881ff1fc7c0c894b04396
Arthur Fontaine <57626613+f-arthr@users.noreply.github.com>
2020-07-20 18:27:52 +0200

    CHANGE ALL !!
    - new design
    - new features

---
+++ js/translating.js
@@ -0,0 +1,242 @@
+var resources = {
+    "en": {
+        "translation": {
+            "homepage": {
+                "download": "Download my resume",
+                "developer": "Developer & Student",
+                "skills": "My skills",
+                "experience": "My experience",
+                "medialib": "My media library",
+                "contact": "Contact me"
+            },
+            "experience": {
+                "magneti": {
+                    "type": "Internship",
+                    "theme": "GLOBAL",
+                    "date": "December 2018",
+                    "more": "Download my report"
+                },
+                "withings": {
+                    "type": "Internship",
+                    "theme": "MARKETING",
+                    "date": "June 2019",
+                    "more": "Download my report"
+                },
+                "beatter": {
+                    "type": "Project",
+                    "date": "Since March 2019",
+                    "more": "View on GitHub"
+                },
+                "opews": {
+                    "type": "Project",
+                    "theme": "NODEJS - CHROME EXTENSION",
+                    "date": "Since June 2019",
+                    "more": "View on GitHub"
+                },
+            },
+            "medialib": {
+                "innovators": {
+                    "type": "Book",
+                    "title": "The Innovators",
+                    "more": "See on Wikipedia"
+                },
+                "elon-musk": {
+                    "type": "Book",
+                    "title": "Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future",
+                    "more": "See on Wikipedia"
+                },
+                "steve-jobs": {
+                    "type": "Book",
+                    "more": "See on Wikipedia"
+                },
+                "on-m-avait-dit": {
+                    "type": "Book",
+                    "title": "I was told that it was impossible",
+                    "more": "See on Wikipedia"
+                },
+                "l-art-de-la-victoire": {
+                    "type": "Book",
+                    "title": "Shoe Dog",
+                    "more": "See on Wikipedia"
+                },
+                "accedez-au-sommet": {
+                    "type": "Book",
+                    "title": "Get to the top the path is within you",
+                    "more": "See on Wikipedia"
+                },
+                "mark-zuckerberg": {
+                    "type": "Book",
+                    "more": "See on Wikipedia"
+                },
+                "permanent-record": {
+                    "type": "Book",
+                    "title": "Permanent record",
+                    "more": "See on Wikipedia"
+                },
+                "le-monde-selon-amazon": {
+                    "type": "Book",
+                    "title": "The world according to Amazon",
+                    "more": "See on Wikipedia"
+                }
+            }
+        }
+    },
+    "fr": {
+        "translation": {
+            "homepage": {
+                "download": "Télécharger mon CV",
+                "developer": "Développeur & Étudiant",
+                "skills": "Mes compétences",
+                "experience": "Mon expérience",
+                "medialib": "Ma médiathèque",
+                "contact": "Me contacter"
+            },
+            "skills": {
+                "title": "Mes compétences",
+                "frontend": {
+                    "title": "Développement front-end"
+                },
+                "backend": {
+                    "title": "Développement back-end"
+                },
+                "mobile": {
+                    "title": "Développement d'application mobile"
+                },
+                "bdandml": {
+                    "title": "Big Data et Machine Learning"
+                },
+                "uiux": {
+                    "title": "UI / UX Design"
+                },
+                "language": {
+                    "title": "Langues",
+                    "items": {
+                        "fr": "Français",
+                        "en": "Anglais",
+                        "de": "Allemand",
+                        "es": "Espagnol"
+                    }
+                },
+                "others": {
+                    "title": "Autres",
+                    "items": {
+                        "bia": "Brevet d'Initiation Aéronautique"
+                    }
+                }
+            },
+            "experience": {
+                "title": "Mon expérience",
+                "magneti": {
+                    "type": "Stage",
+                    "theme": "GLOBAL",
+                    "date": "Décembre 2018",
+                    "more": "Télécharger mon rapport"
+                },
+                "withings": {
+                    "type": "Stage",
+                    "theme": "MARKETING",
+                    "date": "Juin 2019",
+                    "more": "Télécharger mon rapport"
+                },
+                "beatter": {
+                    "type": "Projet",
+                    "date": "Depuis Mars 2019",
+                    "more": "Voir sur GitHub"
+                },
+                "opews": {
+                    "type": "Projet",
+                    "theme": "NODEJS - EXTENSION CHROME",
+                    "date": "Depuis Juin 2019",
+                    "more": "Voir sur GitHub"
+                },
+            },
+            "medialib": {
+                "title": "Ma médiathèque",
+                "innovators": {
+                    "type": "Livre",
+                    "title": "Les Innovateurs",
+                    "more": "Voir sur Wikipedia"
+                },
+                "elon-musk": {
+                    "type": "Livre",
+                    "title": "Elon Musk: Tesla, PayPal, SpaceX : l'entrepreneur qui va changer le monde",
+                    "more": "Voir sur Wikipedia"
+                },
+                "steve-jobs": {
+                    "type": "Livre",
+                    "more": "Voir sur Wikipedia"
+                },
+                "on-m-avait-dit": {
+                    "type": "Livre",
+                    "title": "On m'avait dit que c'était impossible",
+                    "more": "Voir sur Wikipedia"
+                },
+                "l-art-de-la-victoire": {
+                    "type": "Livre",
+                    "title": "L'art de la victoire",
+                    "more": "Voir sur Wikipedia"
+                },
+                "accedez-au-sommet": {
+                    "type": "Livre",
+                    "title": "Accédez au sommet le chemin est en vous",
+                    "more": "Voir sur Wikipedia"
+                },
+                "mark-zuckerberg": {
+                    "type": "Livre",
+                    "more": "Voir sur Wikipedia"
+                },
+                "permanent-record": {
+                    "type": "Livre",
+                    "title": "Mémoires vives",
+                    "more": "Voir sur Wikipedia"
+                },
+                "le-monde-selon-amazon": {
+                    "type": "Livre",
+                    "title": "Le monde selon Amazon",
+                    "more": "Voir sur Wikipedia"
+                }
+            }
+        }
+    }
+}
+
+function getParents(elem) {
+  var parents = [];
+  while(elem.parentNode && elem.parentNode.nodeName.toLowerCase() != 'body') {
+    elem = elem.parentNode;
+    parents.push(elem);
+  }
+  return parents;
+}
+
+function updateContent() {
+    var i18n_el = document.querySelectorAll("[i18n]")
+    i18n_el.forEach(child => {
+        var parents = getParents(child).reverse()
+        var i18n_selector = []
+        parents.forEach(parent => {
+            i18n_parent_name = parent.getAttribute("i18n-parent")
+            if (i18n_parent_name != null) {
+                i18n_selector.push(i18n_parent_name)
+            }
+        })
+        i18n_selector = i18n_selector.join(".") + "." + child.getAttribute("i18n")
+        child.innerHTML = i18next.t(i18n_selector)
+    })
+}
+
+i18next
+    .use(i18nextBrowserLanguageDetector)
+    .init({
+        detection: {
+            order: ['navigator', 'querystring', 'cookie', 'localStorage', 'htmlTag', 'path', 'subdomain']
+        },
+        debug: true,
+        resources: resources
+    }).then(function (t) {
+        updateContent()
+    });
+
+i18next.on('languageChanged', () => {
+    updateContent();
+});
