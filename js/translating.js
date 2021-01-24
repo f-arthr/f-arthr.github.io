97831c255080f98423f986fc4403f21179256f78
arthur-fontaine <0arthur.fontaine@gmail.com>
2021-01-24 16:13:41 +0100

    Upload CV and update Projects

--- js/translating.js
+++ js/translating.js
@@ -1,327 +1,354 @@
 var resources = {
-    "en": {
-        "translation": {
-            "homepage": {
-                "download": "Download my resume",
-                "developer": "Developer & Student",
-                "skills": "My skills",
-                "experience": "My experience",
-                "medialib": "My media library",
-                "contact": "Contact me"
-            },
-            "skills": {
-                "title": "My skills",
-                "frontend": {
-                    "title": "Frontend development"
-                },
-                "backend": {
-                    "title": "Backend development"
-                },
-                "mobile": {
-                    "title": "Mobile application development"
-                },
-                "bdandml": {
-                    "title": "Big Data and Machine Learning"
-                },
-                "uiux": {
-                    "title": "UI / UX Design"
-                },
-                "language": {
-                    "title": "Language",
-                    "items": {
-                        "fr": "French",
-                        "en": "English",
-                        "de": "German",
-                        "es": "Spanish"
-                    }
-                },
-                "others": {
-                    "title": "Others",
-                    "items": {
-                        "bia": "Certificate of Aeronautical Initiation"
-                    }
-                }
-            },
-            "experience": {
-                "title": "My experience",
-                "magneti": {
-                    "type": "Internship",
-                    "theme": "GLOBAL",
-                    "date": "December 2018",
-                    "more": "Download my report"
-                },
-                "withings": {
-                    "type": "Internship",
-                    "theme": "MARKETING",
-                    "date": "June 2019",
-                    "more": "Download my report"
-                },
-                "beatter": {
-                    "type": "Project",
-                    "date": "Since March 2019",
-                    "more": "View on GitHub"
-                },
-                "opews": {
-                    "type": "Project",
-                    "theme": "NODEJS - CHROME EXTENSION",
-                    "date": "Since June 2019",
-                    "more": "View on GitHub"
-                },
-            },
-            "medialib": {
-                "title": "My media library",
-                "innovators": {
-                    "type": "Book",
-                    "title": "The Innovators",
-                    "more": "See on Google Books"
-                },
-                "elon-musk": {
-                    "type": "Book",
-                    "title": "Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future",
-                    "more": "See on Google Books"
-                },
-                "steve-jobs": {
-                    "type": "Book",
-                    "more": "See on Google Books"
-                },
-                "on-m-avait-dit": {
-                    "type": "Book",
-                    "title": "I was told that it was impossible",
-                    "more": "See on Google Books"
-                },
-                "l-art-de-la-victoire": {
-                    "type": "Book",
-                    "title": "Shoe Dog",
-                    "more": "See on Google Books"
-                },
-                "accedez-au-sommet": {
-                    "type": "Book",
-                    "title": "Get to the top the path is within you",
-                    "more": "See on Google Books"
-                },
-                "mark-zuckerberg": {
-                    "type": "Book",
-                    "more": "See on Google Books"
-                },
-                "permanent-record": {
-                    "type": "Book",
-                    "title": "Permanent record",
-                    "more": "See on Google Books"
-                },
-                "le-monde-selon-amazon": {
-                    "type": "Book",
-                    "title": "The world according to Amazon",
-                    "more": "See on Google Books"
-                },
-                "the-valley": {
-                    "type": "Book",
-                    "title": "The Valley",
-                    "more": "See on Google Books"
-                },
-                "tenet": {
-                    "type": "Movie",
-                    "title": "Tenet",
-                    "more": "Watch on YouTube"
-                },
-                "truman-show": {
-                    "type": "Movie",
-                    "title": "The Truman Show",
-                    "more": "Watch on YouTube"
-                },
-                "the-last-dance": {
-                    "type": "Documentary series",
-                    "title": "The Last Dance",
-                    "more": "Watch on Netflix"
-                },
-                "abstract": {
-                    "type": "Documentary series",
-                    "title": "Abstract",
-                    "more": "Watch on Netflix"
-                }
-            }
-        }
-    },
-    "fr": {
-        "translation": {
-            "homepage": {
-                "download": "Télécharger mon CV",
-                "developer": "Développeur & Étudiant",
-                "skills": "Mes compétences",
-                "experience": "Mon expérience",
-                "medialib": "Ma médiathèque",
-                "contact": "Me contacter"
-            },
-            "skills": {
-                "title": "Mes compétences",
-                "frontend": {
-                    "title": "Développement front-end"
-                },
-                "backend": {
-                    "title": "Développement back-end"
-                },
-                "mobile": {
-                    "title": "Développement d'application mobile"
-                },
-                "bdandml": {
-                    "title": "Big Data et Machine Learning"
-                },
-                "uiux": {
-                    "title": "UI / UX Design"
-                },
-                "language": {
-                    "title": "Langues",
-                    "items": {
-                        "fr": "Français",
-                        "en": "Anglais",
-                        "de": "Allemand",
-                        "es": "Espagnol"
-                    }
-                },
-                "others": {
-                    "title": "Autres",
-                    "items": {
-                        "bia": "Brevet d'Initiation Aéronautique"
-                    }
-                }
-            },
-            "experience": {
-                "title": "Mon expérience",
-                "magneti": {
-                    "type": "Stage",
-                    "theme": "GLOBAL",
-                    "date": "Décembre 2018",
-                    "more": "Télécharger mon rapport"
-                },
-                "withings": {
-                    "type": "Stage",
-                    "theme": "MARKETING",
-                    "date": "Juin 2019",
-                    "more": "Télécharger mon rapport"
-                },
-                "beatter": {
-                    "type": "Projet",
-                    "date": "Depuis Mars 2019",
-                    "more": "Voir sur GitHub"
-                },
-                "opews": {
-                    "type": "Projet",
-                    "theme": "NODEJS - EXTENSION CHROME",
-                    "date": "Depuis Juin 2019",
-                    "more": "Voir sur GitHub"
-                },
-            },
-            "medialib": {
-                "title": "Ma médiathèque",
-                "innovators": {
-                    "type": "Livre",
-                    "title": "Les Innovateurs",
-                    "more": "Voir sur Google Books"
-                },
-                "elon-musk": {
-                    "type": "Livre",
-                    "title": "Elon Musk: Tesla, PayPal, SpaceX : l'entrepreneur qui va changer le monde",
-                    "more": "Voir sur Google Books"
-                },
-                "steve-jobs": {
-                    "type": "Livre",
-                    "more": "Voir sur Google Books"
-                },
-                "on-m-avait-dit": {
-                    "type": "Livre",
-                    "title": "On m'avait dit que c'était impossible",
-                    "more": "Voir sur Google Books"
-                },
-                "l-art-de-la-victoire": {
-                    "type": "Livre",
-                    "title": "L'art de la victoire",
-                    "more": "Voir sur Google Books"
-                },
-                "accedez-au-sommet": {
-                    "type": "Livre",
-                    "title": "Accédez au sommet le chemin est en vous",
-                    "more": "Voir sur Google Books"
-                },
-                "mark-zuckerberg": {
-                    "type": "Livre",
-                    "more": "Voir sur Google Books"
-                },
-                "permanent-record": {
-                    "type": "Livre",
-                    "title": "Mémoires vives",
-                    "more": "Voir sur Google Books"
-                },
-                "le-monde-selon-amazon": {
-                    "type": "Livre",
-                    "title": "Le monde selon Amazon",
-                    "more": "Voir sur Google Books"
-                },
-                "the-valley": {
-                    "type": "Livre",
-                    "title": "The Valley",
-                    "more": "Voir sur Google Books"
-                },
-                "tenet": {
-                    "type": "Film",
-                    "title": "Tenet",
-                    "more": "Regarder sur YouTube"
-                },
-                "truman-show": {
-                    "type": "Film",
-                    "title": "The Truman Show",
-                    "more": "Regarder sur YouTube"
-                },
-                "the-last-dance": {
-                    "type": "Série documentaire",
-                    "title": "The Last Dance",
-                    "more": "Regarder sur Netflix"
-                },
-                "abstract": {
-                    "type": "Série documentaire",
-                    "title": "Abstract",
-                    "more": "Regarder sur Netflix"
-                }
-            }
-        }
-    }
-}
+	en: {
+		translation: {
+			homepage: {
+				download: 'Download my resume',
+				developer: 'Developer & Student',
+				skills: 'My skills',
+				experience: 'My experience',
+				medialib: 'My media library',
+				contact: 'Contact me',
+			},
+			skills: {
+				title: 'My skills',
+				frontend: {
+					title: 'Frontend development',
+				},
+				backend: {
+					title: 'Backend development',
+				},
+				mobile: {
+					title: 'Mobile application development',
+				},
+				bdandml: {
+					title: 'Big Data and Machine Learning',
+				},
+				uiux: {
+					title: 'UI / UX Design',
+				},
+				language: {
+					title: 'Language',
+					items: {
+						fr: 'French',
+						en: 'English',
+						de: 'German',
+						es: 'Spanish',
+					},
+				},
+				others: {
+					title: 'Others',
+					items: {
+						bia: 'Certificate of Aeronautical Initiation',
+					},
+				},
+			},
+			experience: {
+				title: 'My experience',
+				magneti: {
+					type: 'Internship',
+					theme: 'GLOBAL',
+					date: 'December 2018',
+					more: 'Download my report',
+				},
+				withings: {
+					type: 'Internship',
+					theme: 'MARKETING',
+					date: 'June 2019',
+					more: 'Download my report',
+				},
+				beatter: {
+					type: 'Project',
+					date: 'Since March 2019',
+					more: 'View on GitHub',
+				},
+				opews: {
+					type: 'Project',
+					theme: 'NODEJS - CHROME EXTENSION',
+					date: 'Since June 2019',
+					more: 'View on GitHub',
+				},
+				hiberfile: {
+					type: 'Contribution',
+					theme: 'JS - WEBCRYPTO',
+					date: 'November 2019',
+					more: 'Go to the website',
+				},
+			},
+			medialib: {
+				title: 'My media library',
+				innovators: {
+					type: 'Book',
+					title: 'The Innovators',
+					more: 'See on Google Books',
+				},
+				'elon-musk': {
+					type: 'Book',
+					title:
+						'Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future',
+					more: 'See on Google Books',
+				},
+				'steve-jobs': {
+					type: 'Book',
+					more: 'See on Google Books',
+				},
+				'on-m-avait-dit': {
+					type: 'Book',
+					title: 'I was told that it was impossible',
+					more: 'See on Google Books',
+				},
+				'l-art-de-la-victoire': {
+					type: 'Book',
+					title: 'Shoe Dog',
+					more: 'See on Google Books',
+				},
+				'accedez-au-sommet': {
+					type: 'Book',
+					title: 'Get to the top the path is within you',
+					more: 'See on Google Books',
+				},
+				'mark-zuckerberg': {
+					type: 'Book',
+					more: 'See on Google Books',
+				},
+				'permanent-record': {
+					type: 'Book',
+					title: 'Permanent record',
+					more: 'See on Google Books',
+				},
+				'le-monde-selon-amazon': {
+					type: 'Book',
+					title: 'The world according to Amazon',
+					more: 'See on Google Books',
+				},
+				'the-valley': {
+					type: 'Book',
+					title: 'The Valley',
+					more: 'See on Google Books',
+				},
+				tenet: {
+					type: 'Movie',
+					title: 'Tenet',
+					more: 'Watch on YouTube',
+				},
+				'truman-show': {
+					type: 'Movie',
+					title: 'The Truman Show',
+					more: 'Watch on YouTube',
+				},
+				'the-last-dance': {
+					type: 'Documentary series',
+					title: 'The Last Dance',
+					more: 'Watch on Netflix',
+				},
+				abstract: {
+					type: 'Documentary series',
+					title: 'Abstract',
+					more: 'Watch on Netflix',
+				},
+			},
+		},
+	},
+	fr: {
+		translation: {
+			homepage: {
+				download: 'Télécharger mon CV',
+				developer: 'Développeur & Étudiant',
+				skills: 'Mes compétences',
+				experience: 'Mon expérience',
+				medialib: 'Ma médiathèque',
+				contact: 'Me contacter',
+			},
+			skills: {
+				title: 'Mes compétences',
+				frontend: {
+					title: 'Développement front-end',
+				},
+				backend: {
+					title: 'Développement back-end',
+				},
+				mobile: {
+					title: "Développement d'application mobile",
+				},
+				bdandml: {
+					title: 'Big Data et Machine Learning',
+				},
+				uiux: {
+					title: 'UI / UX Design',
+				},
+				language: {
+					title: 'Langues',
+					items: {
+						fr: 'Français',
+						en: 'Anglais',
+						de: 'Allemand',
+						es: 'Espagnol',
+					},
+				},
+				others: {
+					title: 'Autres',
+					items: {
+						bia: "Brevet d'Initiation Aéronautique",
+					},
+				},
+			},
+			experience: {
+				title: 'Mon expérience',
+				magneti: {
+					type: 'Stage',
+					theme: 'GLOBAL',
+					date: 'Décembre 2018',
+					more: 'Télécharger mon rapport',
+				},
+				withings: {
+					type: 'Stage',
+					theme: 'MARKETING',
+					date: 'Juin 2019',
+					more: 'Télécharger mon rapport',
+				},
+				beatter: {
+					type: 'Projet',
+					date: 'Depuis Mars 2019',
+					more: 'Voir sur GitHub',
+				},
+				opews: {
+					type: 'Projet',
+					theme: 'NODEJS - EXTENSION CHROME',
+					date: 'Depuis Juin 2019',
+					more: 'Voir sur GitHub',
+				},
+				hiberfile: {
+					type: 'Contribution',
+					theme: 'JS - WEBCRYPTO',
+					date: 'Novembre 2019',
+					more: 'Aller sur le site web',
+				},
+			},
+			medialib: {
+				title: 'Ma médiathèque',
+				innovators: {
+					type: 'Livre',
+					title: 'Les Innovateurs',
+					more: 'Voir sur Google Books',
+				},
+				'elon-musk': {
+					type: 'Livre',
+					title:
+						"Elon Musk: Tesla, PayPal, SpaceX : l'entrepreneur qui va changer le monde",
+					more: 'Voir sur Google Books',
+				},
+				'steve-jobs': {
+					type: 'Livre',
+					more: 'Voir sur Google Books',
+				},
+				'on-m-avait-dit': {
+					type: 'Livre',
+					title: "On m'avait dit que c'était impossible",
+					more: 'Voir sur Google Books',
+				},
+				'l-art-de-la-victoire': {
+					type: 'Livre',
+					title: "L'art de la victoire",
+					more: 'Voir sur Google Books',
+				},
+				'accedez-au-sommet': {
+					type: 'Livre',
+					title: 'Accédez au sommet le chemin est en vous',
+					more: 'Voir sur Google Books',
+				},
+				'mark-zuckerberg': {
+					type: 'Livre',
+					more: 'Voir sur Google Books',
+				},
+				'permanent-record': {
+					type: 'Livre',
+					title: 'Mémoires vives',
+					more: 'Voir sur Google Books',
+				},
+				'le-monde-selon-amazon': {
+					type: 'Livre',
+					title: 'Le monde selon Amazon',
+					more: 'Voir sur Google Books',
+				},
+				'the-valley': {
+					type: 'Livre',
+					title: 'The Valley',
+					more: 'Voir sur Google Books',
+				},
+				tenet: {
+					type: 'Film',
+					title: 'Tenet',
+					more: 'Regarder sur YouTube',
+				},
+				'truman-show': {
+					type: 'Film',
+					title: 'The Truman Show',
+					more: 'Regarder sur YouTube',
+				},
+				'the-last-dance': {
+					type: 'Série documentaire',
+					title: 'The Last Dance',
+					more: 'Regarder sur Netflix',
+				},
+				abstract: {
+					type: 'Série documentaire',
+					title: 'Abstract',
+					more: 'Regarder sur Netflix',
+				},
+			},
+		},
+	},
+};
 
 function getParents(elem) {
-  var parents = [];
-  while(elem.parentNode && elem.parentNode.nodeName.toLowerCase() != 'body') {
-    elem = elem.parentNode;
-    parents.push(elem);
-  }
-  return parents;
+	var parents = [];
+	while (
+		elem.parentNode &&
+		elem.parentNode.nodeName.toLowerCase() != 'body'
+	) {
+		elem = elem.parentNode;
+		parents.push(elem);
+	}
+	return parents;
 }
 
 function updateContent() {
-    var i18n_el = document.querySelectorAll("[i18n]")
-    i18n_el.forEach(child => {
-        var parents = getParents(child).reverse()
-        var i18n_selector = []
-        parents.forEach(parent => {
-            i18n_parent_name = parent.getAttribute("i18n-parent")
-            if (i18n_parent_name != null) {
-                i18n_selector.push(i18n_parent_name)
-            }
-        })
-        i18n_selector = i18n_selector.join(".") + "." + child.getAttribute("i18n")
-        child.innerHTML = i18next.t(i18n_selector)
-    })
+	var i18n_el = document.querySelectorAll('[i18n]');
+	i18n_el.forEach((child) => {
+		var parents = getParents(child).reverse();
+		var i18n_selector = [];
+		parents.forEach((parent) => {
+			i18n_parent_name = parent.getAttribute('i18n-parent');
+			if (i18n_parent_name != null) {
+				i18n_selector.push(i18n_parent_name);
+			}
+		});
+		i18n_selector =
+			i18n_selector.join('.') + '.' + child.getAttribute('i18n');
+		child.innerHTML = i18next.t(i18n_selector);
+	});
 }
 
 i18next
-    .use(i18nextBrowserLanguageDetector)
-    .init({
-        detection: {
-            order: ['navigator', 'querystring', 'cookie', 'localStorage', 'htmlTag', 'path', 'subdomain']
-        },
-        debug: true,
-        resources: resources
-    }).then(function (t) {
-        updateContent()
-    });
+	.use(i18nextBrowserLanguageDetector)
+	.init({
+		detection: {
+			order: [
+				'navigator',
+				'querystring',
+				'cookie',
+				'localStorage',
+				'htmlTag',
+				'path',
+				'subdomain',
+			],
+		},
+		debug: true,
+		resources: resources,
+	})
+	.then(function (t) {
+		updateContent();
+	});
 
 i18next.on('languageChanged', () => {
-    updateContent();
+	updateContent();
 });
